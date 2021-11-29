248a64aa27a7a1463df8b6969a75489f66d1bff9
majaa12 <majacol98@gmail.com>
2021-11-29 20:28:04 +0100

    Dokumentovani enum-i Nivo i TipKursa i dodat plugin za javadoc.

--- src/main/java/rs/ac/bg/fon/nprog/common/domain/Nivo.java
+++ src/main/java/rs/ac/bg/fon/nprog/common/domain/Nivo.java
@@ -2,7 +2,42 @@ package rs.ac.bg.fon.nprog.common.domain;
 
 import java.io.Serializable;
 
-public enum Nivo implements Serializable {
+/**
+ * Enum koji predstavlja moguce nivoe znanja nekog stranog jezika koji se uci na
+ * kursu.
+ * 
+ * @author Maja
+ * @version 0.1
+ */
 
-	A1, A2, B1, B2, C1, C2;
+public enum Nivo implements Serializable {
+	/**
+	 * Nivo kursa A1
+	 */
+	A1,
+
+	/**
+	 * Nivo kursa A2
+	 */
+	A2,
+	
+	/**
+	 * Nivo kursa B1
+	 */
+	B1,
+	
+	/**
+	 * Nivo kursa B2
+	 */
+	B2,
+	
+	/**
+	 * Nivo kursa C1
+	 */
+	C1,
+	
+	/**
+	 * Nivo kursa C2
+	 */
+	C2;
 }
--- src/main/java/rs/ac/bg/fon/nprog/common/domain/TipKursa.java
+++ src/main/java/rs/ac/bg/fon/nprog/common/domain/TipKursa.java
@@ -2,8 +2,35 @@ package rs.ac.bg.fon.nprog.common.domain;
 
 import java.io.Serializable;
 
+/**
+ * 
+ * Enum koji predstavlja moguce tipove kurseva koji se pohadjaju u razlicite
+ * svrhe.
+ * 
+ * @author Maja
+ * @version 0.1
+ */
+
 public enum TipKursa implements Serializable {
 
-	Poslovni, Konverzacijski, Deciji, Opsti;
+	/**
+	 * Poslovni tip kursa
+	 */
+	Poslovni,
+	
+	/**
+	 * Konverzacijski tip kursa
+	 */
+	Konverzacijski,
+	
+	/**
+	 * Deciji tip kursa
+	 */
+	Deciji,
+	
+	/**
+	 * Opsti tip kursa
+	 */
+	Opsti;
 
 }
