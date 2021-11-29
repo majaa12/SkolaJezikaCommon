8beb266f3306257ca4d77ce1cd12d7f826dd3576
majaa12 <majacol98@gmail.com>
2021-11-29 23:08:09 +0100

    Dokumentovan interfejs GenericEntity i klasa Administrator.

--- src/main/java/rs/ac/bg/fon/nprog/common/domain/Administrator.java
+++ src/main/java/rs/ac/bg/fon/nprog/common/domain/Administrator.java
@@ -5,17 +5,62 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+/**
+ * Klasa koja predstavlja administratora sistema i implementira interfejs
+ * GenericEntity
+ *
+ * Administrator ima svoj IDAdministratora tipa long, kao i ime, prezime,
+ * korisnickoIme i lozinku kao String vrednosti.
+ * 
+ * @author Maja
+ * @version 0.1
+ */
+
 public class Administrator implements GenericEntity {
 
+	/**
+	 * IDAdministratora tipa long.
+	 */
 	private long IDAdministratora;
+
+	/**
+	 * Ime tipa String.
+	 */
 	private String ime;
+
+	/**
+	 * Prezime tipa String.
+	 */
 	private String prezime;
+
+	/**
+	 * Korisnicko ime tipa String.
+	 */
 	private String korisnickoIme;
+
+	/**
+	 * Lozinka tipa String.
+	 */
 	private String lozinka;
 
+	/**
+	 * Besparametarski konstruktor koji inicijalizuje objekat klase Administrator i
+	 * nista vise.
+	 */
 	public Administrator() {
 	}
 
+	/**
+	 * Parametarski konstruktor koji inicijalizuje objekat klase Administrator i
+	 * postavlja pocetne vrednosti za atribute IDAdministratora, ime, prezime,
+	 * korisnickoIme i lozinka
+	 * 
+	 * @param IDAdministratora Id administratora kao long
+	 * @param ime              Ime administratora kao String
+	 * @param prezime          Prezime administratora kao String
+	 * @param korisnickoIme    Korisnicko ime administratora kao String
+	 * @param lozinka          Lozinka administratora kao String
+	 */
 	public Administrator(long IDAdministratora, String ime, String prezime, String korisnickoIme, String lozinka) {
 		this.IDAdministratora = IDAdministratora;
 		this.ime = ime;
@@ -24,10 +69,23 @@ public class Administrator implements GenericEntity {
 		this.lozinka = lozinka;
 	}
 
+	/**
+	 * Vraca lozinku administratora.
+	 * 
+	 * @return Lozinka administratora kao String vrednost.
+	 */
 	public String getLozinka() {
 		return lozinka;
 	}
 
+	/**
+	 * Postavlja lozinku administratora ako ona nije null i nije prazan String.
+	 * 
+	 * @param lozinka Lozinka administratora kao String vrednost.
+	 * @throws java.lang.NullPointerException ako je prosledjena lozinka null.
+	 * @throws java.lang.RuntimeException     ako je prosledjena lozinka prazan
+	 *                                        String.
+	 */
 	public void setLozinka(String lozinka) {
 		if (lozinka == null) {
 			throw new NullPointerException("Lozinka ne sme biti null vrednost!");
@@ -38,18 +96,40 @@ public class Administrator implements GenericEntity {
 		this.lozinka = lozinka;
 	}
 
+	/**
+	 * Vraca id administratora.
+	 * 
+	 * @return IDAdministratora kao long
+	 */
 	public long getIDAdministratora() {
 		return IDAdministratora;
 	}
 
+	/**
+	 * Postavlja lozinku administratora.
+	 * 
+	 * @param IDAdministratora Id administratora kao long.
+	 */
 	public void setIDAdministratora(long IDAdministratora) {
 		this.IDAdministratora = IDAdministratora;
 	}
 
+	/**
+	 * Vraca ime administratora.
+	 * 
+	 * @return ime administratora kao String.
+	 */
 	public String getIme() {
 		return ime;
 	}
 
+	/**
+	 * Postavlja ime administratora ako ono nije null i nije prazan String.
+	 * 
+	 * @param ime Ime administratora kao String.
+	 * @throws java.lang.NullPointerException ako je prosledjeno ime null.
+	 * @throws java.lang.RuntimeException     ako je prosledjeno ime prazan String.
+	 */
 	public void setIme(String ime) {
 		if (ime == null) {
 			throw new NullPointerException("Ime ne sme biti null vrednost!");
@@ -60,34 +140,65 @@ public class Administrator implements GenericEntity {
 		this.ime = ime;
 	}
 
+	/**
+	 * Vraca prezime administratora.
+	 * 
+	 * @return prezime administratora kao String.
+	 */
 	public String getPrezime() {
 		return prezime;
 	}
 
+	/**
+	 * Postavlja prezime administratora ako ono nije null i nije prazan String.
+	 * 
+	 * @param prezime Prezime administratora kao String.
+	 * @throws java.lang.NullPointerException ako je prosledjeno prezime null.
+	 * @throws java.lang.RuntimeException     ako je prosledjeno prezime prazan
+	 *                                        String.
+	 */
 	public void setPrezime(String prezime) {
 		if (prezime == null) {
 			throw new NullPointerException("Prezime ne sme biti null vrednost!");
 		}
 		if (prezime.isEmpty()) {
 			throw new RuntimeException("Prezime ne sme biti prazan string!");
-		}		
+		}
 		this.prezime = prezime;
 	}
 
+	/**
+	 * Vraca korisnicko ime administratora.
+	 * 
+	 * @return korisnickoIme administratora kao String.
+	 */
 	public String getKorisnickoIme() {
 		return korisnickoIme;
 	}
 
+	/**
+	 * Postavlja korisnicko ime administratora ako ono nije null i nije prazan
+	 * String.
+	 * 
+	 * @param korisnickoIme Korisnicko ime administratora kao String.
+	 * @throws java.lang.NullPointerException ako je prosledjeno korisnicko ime
+	 *                                        null.
+	 * @throws java.lang.RuntimeException     ako je prosledjeno korisnicko ime
+	 *                                        prazan String.
+	 */
 	public void setKorisnickoIme(String korisnickoIme) {
 		if (korisnickoIme == null) {
 			throw new NullPointerException("Korisnicko ime ne sme biti null vrednost!");
 		}
 		if (korisnickoIme.isEmpty()) {
 			throw new RuntimeException("Korisnicko ime ne sme biti prazan string!");
-		}		
+		}
 		this.korisnickoIme = korisnickoIme;
 	}
 
+	/**
+	 * Vraca ime i prezime administratora kao jedan String.
+	 */
 	@Override
 	public String toString() {
 		return ime + " " + prezime;
@@ -101,6 +212,18 @@ public class Administrator implements GenericEntity {
 		return hash;
 	}
 
+	/**
+	 * Poredi dva administratora i vraca true ako su isti, a false ako nisu.
+	 * Administratori se porede po korisnickom imenu i lozinki i oba moraju da budu
+	 * ista.
+	 * 
+	 * @return
+	 *         <ul>
+	 *         <li>true ako su oba objekta klase Administrator i imaju ista
+	 *         korisnicka imena i lozinke
+	 *         <li>false u svim ostalim slucajevima
+	 *         </ul>
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj) {
--- src/main/java/rs/ac/bg/fon/nprog/common/domain/GenericEntity.java
+++ src/main/java/rs/ac/bg/fon/nprog/common/domain/GenericEntity.java
@@ -4,27 +4,99 @@ import java.io.Serializable;
 import java.sql.ResultSet;
 import java.util.List;
 
+/**
+ * 
+ * Javni interfejs koji predstavlja opsti entitet koga implementiraju sve
+ * domenske klase sistema.
+ * 
+ * @author Maja
+ * @version 0.1
+ */
 public interface GenericEntity extends Serializable {
 
+	/**
+	 * Vraca naziv tabele iz baze kao String.
+	 * 
+	 * @return Naziv tabele kao String.
+	 */
 	String getTableName();
 
+	/**
+	 * Vraca nazive kolona tabele u koje ce biti upisana neka vrednost.
+	 * 
+	 * @return Nazivi kolona tabele kao jedan String.
+	 */
 	String getColumnNamesForInsert();
 
+	/**
+	 * Vraca konkretne vrednosti za kolone tabele koje treba ubaciti.
+	 * 
+	 * @return Vrednosti kolona tabele kao jedan String.
+	 */
 	String getInsertValues();
 
+	/**
+	 * Postavlja vrednost id-a tabele iz baze kao vrednost id-a odredjene domenske
+	 * klase.
+	 * 
+	 * @param id Id izvucen iz baze kao Long.
+	 */
 	void setId(Long id);
 
+	/**
+	 * Vraca uslov pretrage za 'where' deo upita koji treba jedinstveno da
+	 * identifikuje slog tabele.
+	 * 
+	 * @return Naziv kolone tabele i njenu vrednost za jedan slog u bazi kao String.
+	 */
 	String getConditionForOne();
 
+	/**
+	 * Vraca nazive kolona tabele i njihove nove vrednosti kako bi se podaci
+	 * azurirali.
+	 * 
+	 * @return Nazive kolona tabele i njihove vrednosti za jedan slog kao String.
+	 */
 	String setUpdateValues();
 
+	/**
+	 * 
+	 * @param rs Slogovi iz baze kao ResultSet.
+	 * @return GenericEntity (Opsti entitet) dobijen materijalizacijom iz
+	 *         ResultSeta.
+	 * @throws Exception ako je rs null i trazeni objekat ne postoji u bazi.
+	 */
 	GenericEntity fillFromRS(ResultSet rs) throws Exception;
 
+	/**
+	 * 
+	 * @param rs Slogovi iz baze kao ResultSet.
+	 * @return lista GenericEntity (opstih entiteta) dobijenih materijalizacijom iz
+	 *         ResultSeta.
+	 * @throws Exception ako je rs null i nema rezultata pretrage.
+	 */
 	List<GenericEntity> fillListFromRS(ResultSet rs) throws Exception;
 
+	/**
+	 * Vraca uslov za spajanje dve ili vise tabela.
+	 * 
+	 * @return Join uslov kao String.
+	 */
 	String getJoinCondition();
 
+	/**
+	 * Vraca uslov pretrage za 'where' deo upita koji moze biti po vise kriterijuma
+	 * i vise slogova tabele mogu zadovoljiti te kriterijume.
+	 * 
+	 * @return Nazive kolona tabele i njihove vrednosti za jedan slog u bazi kao
+	 *         String.
+	 */
 	String getConditionForMore();
 
+	/**
+	 * Vraca uslov za sortiranje rezultata upita.
+	 * 
+	 * @return Order by uslov kao String.
+	 */
 	String getSort();
 }
